2534c8b25d79367cc0e4b1b1a7917b04b30a9f39
dmadhu123 <dulikamadushani1234@gmail.com>
2022-02-27 11:55:47 +0530

    modify expenseList and App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
-import ExpenseList from './component/ExpenceList';
+import ExpenseList from './component/ExpenseList';
 import ExpenseForm from './component/ExpenseForm';
 import Alert from './component/Alert';
+
+const initialExpenses = [
+  {id:1, charge: "rent", amount:1600},
+  {id:2, charge: "car payment", amount:400},
+  {id:3, charge: "credit card bill", amount:1200}
+]
+
 
 function App() {
   return (
